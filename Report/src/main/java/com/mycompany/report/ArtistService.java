1325c185653dc5211b3b9d44d58c30ec52b18af8
Titapon Keawbua <64160047@go.buu.ac.th>
2023-10-23 22:03:42 +0700

    artistService

---
+++ Report/src/main/java/com/mycompany/report/ArtistService.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.report;
+
+import java.util.List;
+
+/**
+ *
+ * @author toey
+ */
+public class ArtistService {
+
+    public List<ArtistReport> getTopTenArtistByTotalPrice() {
+        ArtistDao artistDao = new ArtistDao();
+        return artistDao.getArtistByTotalPrice(10);
+    }
+
+    public List<ArtistReport> getTopTenArtistByTotalPrice(String begin, String end) {
+        ArtistDao artistDao = new ArtistDao();
+        return artistDao.getArtistByTotalPrice(begin, end, 10);
+    }
+}
