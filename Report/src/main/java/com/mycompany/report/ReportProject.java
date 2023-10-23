48841fdf6fc758adeab2dddd6dd600d1c75e874d
Titapon Keawbua <64160047@go.buu.ac.th>
2023-10-23 22:05:06 +0700

    Report project

---
+++ Report/src/main/java/com/mycompany/report/ReportProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.report;
+
+/**
+ *
+ * @author toey
+ */
+public class ReportProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
