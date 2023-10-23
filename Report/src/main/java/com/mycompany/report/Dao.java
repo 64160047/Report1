ddad5b8c6c466387db5a3ad0d82dbbd435e13968
Titapon Keawbua <64160047@go.buu.ac.th>
2023-10-23 22:02:04 +0700

    ArtistDao

---
+++ Report/src/main/java/com/mycompany/report/Dao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.report;
+
+import java.util.List;
+
+/**
+ *
+ * @author toey
+ */
+public interface Dao<T> {
+    T get(int id);
+    List<T> getAll();
+    T save(T obj);
+    T update(T obj);
+    int delete(T obj);
+    List<T> getAll(String where, String order);
+}
